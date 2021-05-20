c733b6c6039b81eccd94ecf307bd32f8b75f37cb
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-20 21:42:59 +0900

    movie info type 정의

--- front/src/typings/db.ts
+++ front/src/typings/db.ts
@@ -25,3 +25,54 @@ export interface IMovie {
   vote_average: number;
   vote_count: number;
 }
+
+export interface IMoveInfo {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: null;
+  budget: number;
+  genres: IGenre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: IProductionCompanie[];
+  production_countries: IProductionCountries[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: ISpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IGenre {
+  id: number;
+  name: string;
+}
+
+export interface IProductionCompanie {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface IProductionCountries {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface ISpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
