e32dfadda14280f333d7172bff4018cfd67b57c1
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-20 21:14:13 +0900

    movie detail page route 연결

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from "@pages/LoginPage";
 import RegisterPage from "@pages/RegisterPage";
 import Auth from "@hoc/Auth";
 import NavBar from "@components/NavBar";
+import MovieDetailPage from "@pages/MovieDetailPage";
 
 function App() {
   return (
@@ -15,6 +16,10 @@ function App() {
           <Route path="/" exact component={Auth(LandingPage, null)} />
           <Route path="/login" component={Auth(LoginPage, false)} />
           <Route path="/register" component={Auth(RegisterPage, false)} />
+          <Route
+            path="/movie/:movieId"
+            component={Auth(MovieDetailPage, null)}
+          />
         </Switch>
       </div>
     </Suspense>
