f758ba1622c0758e7b8af8438555bec0aef40e46
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-21 20:26:54 +0900

    common으로 사용하기위한 처리

--- front/src/components/GridCards/index.tsx
+++ front/src/components/GridCards/index.tsx
@@ -2,17 +2,26 @@ import React from "react";
 import { Col } from "antd";
 import { Link } from "react-router-dom";
 import { CardImage, ImageContainer } from "./styles";
+
+type ContentType = "movie" | "actor";
+
 interface IGridCardsProps {
   image: string;
-  movieId: number;
-  movieName: string;
+  contentId: number;
+  contentName: string;
+  content: ContentType;
 }
-function GridCards({ image, movieId, movieName }: IGridCardsProps) {
+function GridCards({
+  image,
+  contentId,
+  contentName,
+  content,
+}: IGridCardsProps) {
   return (
     <Col lg={6} md={8} xs={24}>
       <ImageContainer>
-        <Link to={`/movie/${movieId}`}>
-          <CardImage src={image} alt={movieName} />
+        <Link to={`/${content}/${contentId}`}>
+          <CardImage src={image} alt={contentName} />
         </Link>
       </ImageContainer>
     </Col>
